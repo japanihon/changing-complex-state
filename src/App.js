9daf7cd13523bbc5b2152e2518dccfa24c622a7c
andrustn85 <rustandres@gmail.com>
2022-08-07 17:52:02 +0200

    solucion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import React, { useState } from 'react';
 
 import './App.css';
 
 export default function App() {
+
+const [fName, setFName] = useState("");
+
+const [lName, setLName] = useState(""); 
+
+function updateFName(event) {
+  const firstName = event.target.value;
+  setFName(firstName);
+}
+
+function updateLName(event) {
+  const lastName = event.target.value;
+  setLName(lastName);
+}
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1> Hello {fName} {lName} </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input 
+        name="fName" 
+        placeholder="First Name"
+        value={fName}
+        onChange={updateFName}
+         />
+        <input 
+        name="lName" 
+        placeholder="Last Name" 
+        value={lName}
+        onChange={updateLName}
+        />
+        <button > Submit </button>
       </form>
     </div>
   );
